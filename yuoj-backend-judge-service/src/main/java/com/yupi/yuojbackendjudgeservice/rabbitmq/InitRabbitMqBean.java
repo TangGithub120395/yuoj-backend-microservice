2ab1d09c1cd2808c07b5b433f60c40eb6899a80c
tangzhen <tangzhen020515@163.com>
2024-03-07 17:21:17 +0800

    完成Docker部署项目

--- yuoj-backend-judge-service/src/main/java/com/yupi/yuojbackendjudgeservice/rabbitmq/InitRabbitMqBean.java
+++ yuoj-backend-judge-service/src/main/java/com/yupi/yuojbackendjudgeservice/rabbitmq/InitRabbitMqBean.java
@@ -10,16 +10,26 @@ import com.rabbitmq.client.Channel;
 import com.rabbitmq.client.Connection;
 import com.rabbitmq.client.ConnectionFactory;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.stereotype.Component;
+
+import javax.annotation.PostConstruct;
 
 /**
  * 用于创建测试程序用到的交换机和队列（只用在程序启动前执行一次）
  */
 @Slf4j
-public class InitRabbitMq {
-    public static void doInit(){
+@Component
+public class InitRabbitMqBean {
+    @Value("${spring.rabbitmq.host:localhost}")
+    private String host;
+
+    @PostConstruct
+    public void doInit(){
         try {
             ConnectionFactory factory = new ConnectionFactory();
-            factory.setHost("localhost");
+            factory.setHost(host);
             Connection connection = factory.newConnection();
             Channel channel = connection.createChannel();
             String EXCHANGE_NAME = "code_exchange";
@@ -29,7 +39,7 @@ public class InitRabbitMq {
             String queueName = "code_queue";
             channel.queueDeclare(queueName, true, false, false, null);
             channel.queueBind(queueName, EXCHANGE_NAME, "my_routingKey");
-            log.info("doInit方法执行,RabbitMq启动成功");
+            log.info("Bean的doInit方法执行,RabbitMq启动成功");
         } catch (Exception e) {
             log.error("RabbitMq启动失败");
         }
--- yuoj-backend-question-service/src/test/java/com/yupi/yuojbackendquestionservice/YuojBackendQuestionServuceApplicationTests.java
+++ yuoj-backend-question-service/src/test/java/com/yupi/yuojbackendquestionservice/YuojBackendQuestionServuceApplicationTests.java
@@ -3,7 +3,7 @@ package com.yupi.yuojbackendquestionservice;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+@SpringBootTest(classes = {YuojBackendQuestionServuceApplication.class})
 class YuojBackendQuestionServuceApplicationTests {
 
     @Test
